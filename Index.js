ae971c523f488b4a3b3d9e06a7cacd8a43c89360
kramzssis <90599674+kramzssis@users.noreply.github.com>
2022-11-27 00:45:54 +0800

    Web Page with Java

---
+++ Index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Computer Technician", "Software Developer","Cyber Security", "White Hat" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+    
+    })
+    
